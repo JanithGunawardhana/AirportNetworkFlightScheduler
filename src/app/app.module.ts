3efaeb62f04dade7251ca7e0f8fdb49f029f4c37
Prabhanu <prabhanu96@gmail.com>
2019-11-06 15:46:08 +0530

    imported new modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from './material/material.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DisplayComponent } from './display/display.component';
+import { FlightComponent } from './display/flight/flight.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DisplayComponent
+    DisplayComponent,
+    FlightComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [FlightComponent],
+  entryComponents: [FlightComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
