cd4be029f26cc0864620258461c060a16112a541
Prabhanu <prabhanu96@gmail.com>
2019-11-06 15:45:24 +0530

    added new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DisplayComponent } from './display/display.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: '', component: DisplayComponent},
+  {path: ':admin', component: DisplayComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
