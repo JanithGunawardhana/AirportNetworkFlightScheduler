0ca738e33150b6a584432f2ef7ddbce4c5f100a0
Prabhanu <prabhanu96@gmail.com>
2019-11-06 15:47:36 +0530

    added session and login interface

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { FlightComponent } from './flight/flight.component';
 
 @Component({
   selector: 'app-display',
@@ -6,10 +9,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./display.component.css']
 })
 export class DisplayComponent implements OnInit {
+  isAdmin = false;
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              public dialog: MatDialog,) {
 
-  constructor() { }
+  console.log(localStorage.getItem('loginStatus') == 'true');
+  if ( localStorage.getItem('loginStatus') == 'true' ) {
+    this.isAdmin = true;
+  }
+}
 
   ngOnInit() {
   }
 
+  onLoginAsAdmin() {
+    console.log("login function");
+    return this.router.navigate(['/login/']);
+  }
+
+  onSignOut() {
+    console.log("sign out function");
+    this.isAdmin = false;
+    localStorage.removeItem('loginStatus');
+  }
+
+  onAddNewFlight() {
+    console.log("add new");
+    this.openDialog();
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(FlightComponent, {
+      width: '450px',
+      data: {}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      console.log(result);
+    });
+  }
 }
