f364c6a8d0a1327ecad47c9764a089662a618373
Prabhanu <prabhanu96@gmail.com>
2019-11-06 15:48:06 +0530

    added session and login interface

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  hide = true;
+  correctUsername = 'admin';
+  correctPassword = 'password';
+  username = '';
+  password = '';
+  constructor(private router : Router) { }
 
   ngOnInit() {
+  }
+
+  onLogin() {
+    console.log("login button");
+    if ( this.username.trim() === '' ) {
+      alert("Enter Username");
+    } else if ( this.password.trim() === '' ){
+      alert("Enter password");
+    }else if (this.username.trim() === this.correctUsername && this.password === this.correctPassword) {
+      console.log("login success");
+      localStorage.setItem('loginStatus','true');
+      return this.router.navigate(['']);
+    } else {
+      alert("Enter correct Username and Password")
+    } 
   }
 
 }
